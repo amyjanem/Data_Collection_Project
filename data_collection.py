dd6170cc73b72c651425b53734f75b2fa2021630
Amy Mallett <amymallett8@gmail.com>
2022-11-21 21:43:14 +0000

    protected methods added

--- data_collection.py
+++ data_collection.py
@@ -1,13 +1,13 @@
+from datetime import datetime
+import json
+import os
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from datetime import datetime
 import time
 import uuid
-import os
-import json
-import requests
 
 
 class Webscraper:
@@ -18,7 +18,7 @@ class Webscraper:
         self.driver.get(url)
         self.driver.maximize_window()
     
-    
+
     def click_element(self, xpath: str):
         '''
         Finds a specific element on the webpage and clicks it.
@@ -26,12 +26,14 @@ class Webscraper:
         Parameters
         ---------
         xpath: str
-            The xpath of the element to be clicked
+            The xpath of the element to be clicked.
         '''
         time.sleep(2)
 
         element = self.driver.find_element(By.XPATH, xpath)
         element.click()
+
+        return element
 
 
     def find_element_in_container(self, xpath_container: str, tag_elements: str) -> list:
@@ -41,10 +43,10 @@ class Webscraper:
         Parameters
         ----------
         xpath_container: str
-            The xpath of the container
+            The xpath of the container.
         
         tag_elements: str
-            The tag for the elements within the container
+            The tag for the elements within the container.
         '''
         container = self.driver.find_element(By.XPATH, xpath_container)
         elements_in_container = container.find_elements(By.XPATH, f'./{tag_elements}')
@@ -66,61 +68,68 @@ class Webscraper:
 
     def close_email_signup(self, xpath: str = '//button[@class="emailReengagement_close_button"]'):
         '''
-        Open MyProtein and close email newletter sign up pop-up
+        Open MyProtein and close email newletter sign up pop-up.
 
-        Returns
+        Parameters:
         -------
-        driver: webdriver.Chrome
-            This driver is already in the MyProtein webpage
+        xpath: 
+            The xpath of the 'X' button to close the pop-up.
+
         '''       
         try:
-            self.click_element(xpath)
+            email_signup_button = self.click_element(xpath)
         except:
             pass
-    
+            
+        return email_signup_button
+
 
     def accept_cookies(self, xpath: str = '//button[@class="cookie_modal_button"]'):
         '''
-        Accepts the cookies on the webpage
+        Accepts the cookies on the webpage.
         
         Parameters
         ----------
         xpath: str
-            The xpath of the "Accept Cookies" button
+            The xpath of the "Accept Cookies" button.
         '''
         try:
-            self.click_element(xpath)
+            accept_cookies_button = self.click_element(xpath)
         except:
             pass
+
+        return accept_cookies_button
 
 
     def click_next_page(self, xpath: str = '//button[@class="responsivePaginationNavigationButton paginationNavigationButtonNext"]'):
         '''
-        Clicks the 'Next' button to navigate to the next webpage
+        Clicks the 'Next' button to navigate to the next webpage.
 
         Parameters
         ----------
         xpath: str
-            The xpath of the "Next" button
+            The xpath of the "Next" button.
         '''
         try:
-            self.click_element(xpath)
+            next_page_button = self.click_element(xpath)
         except:
             pass
+
+        return next_page_button
 
 
 
 class MyProteinScraper(Webscraper):    
 
 
-    def nutrition_button_click(self, xpath: str = '//a[@class="responsiveFlyoutMenu_levelOneLink responsiveFlyoutMenu_levelOneLink-hasChildren"]'):
+    def _nutrition_button_click(self, xpath: str = '//a[@class="responsiveFlyoutMenu_levelOneLink responsiveFlyoutMenu_levelOneLink-hasChildren"]'):
         '''
-        Finds 'Nutrition' catergory and clicks it.
+        Finds 'Nutrition' catergory button and clicks it.
 
         Parameters:
         -----------
         xpath: str
-            The xpath of the nutrition button
+            The xpath of the nutrition button.
         '''   
         time.sleep(1)
         nutrition_button = self.click_element(xpath)
@@ -128,14 +137,14 @@ class MyProteinScraper(Webscraper):
         return nutrition_button
         
 
-    def open_all_nutrition_products(self, xpath: str = '//a[@class="sectionPeek_allCta sectionPeek_allCta-show"]'):
+    def _open_all_nutrition_products(self, xpath: str = '//a[@class="sectionPeek_allCta sectionPeek_allCta-show"]'):
         '''
         Clicks 'View All' button so that all Bestseller products are showing.
 
         Parameters:
         -----------
         xpath: str
-            Xpath of the 'View All' button on the NUtrition webpage
+            Xpath of the 'View All' button on the NUtrition webpage.
         '''
         time.sleep(1)
         self.driver.execute_script("window.scrollTo(0, 1300)")
@@ -145,9 +154,9 @@ class MyProteinScraper(Webscraper):
         return nutrition_view_all
         
 
-    def find_product_links(self) -> list:
+    def _find_product_links(self) -> list:
         '''
-        Gets links to all products and stores the links to these in a list (product_link_list)
+        Gets links to all products and stores these in a list.
         '''
         product_link_list = []
 
@@ -164,9 +173,9 @@ class MyProteinScraper(Webscraper):
         return product_link_list
 
 
-    def first_product_click(self):
+    def _first_product_click(self):
         '''
-        Clicks on the first product on the page. (for testing purposes for now!)
+        Clicks on the first product on the page. 
         '''
         time.sleep(1)
         first_product_link = self.click_element('//a[@class="athenaProductBlock_linkImage"]')
@@ -175,9 +184,9 @@ class MyProteinScraper(Webscraper):
         return first_product_link
 
 
-    def get_product_image(self):
+    def _get_product_image(self):
         '''
-        Finds the href to the product image.
+        Finds the 'href' (ie. link) to the product image.
         '''
         time.sleep(1)
         product_image = self.driver.find_element(By.XPATH, '//img[@class="athenaProductImageCarousel_image"]').get_attribute('src')
@@ -186,9 +195,9 @@ class MyProteinScraper(Webscraper):
 
 
     @staticmethod
-    def get_timestamp():
+    def _get_timestamp():
         '''
-        Prints the timestamp of current time.
+        Prints the current timestamp.
         '''
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
@@ -197,7 +206,7 @@ class MyProteinScraper(Webscraper):
 
 
     @staticmethod
-    def get_date_and_timestamp():
+    def _get_date_and_timestamp():
         '''
         Prints the current date and time.
         '''
@@ -207,17 +216,11 @@ class MyProteinScraper(Webscraper):
         return full_datestamp
 
 
-    def get_product_data(self):
+    def _get_product_data(self):
         '''
         Finds xpath of product name, price, and rating of product and creates a dictionary of all the data.
-
-        Parameters:
-        -----------
-        product_link: str
-            the xpath of the url link to an individual product
         '''       
         product_name = self.driver.find_element(By.XPATH, '//h1[@class="productName_title"]').text 
-        
         product_price = self.driver.find_element(By.XPATH, '//p[@class="productPrice_price  "]').text
 
         try:
@@ -229,20 +232,20 @@ class MyProteinScraper(Webscraper):
         return product_name, product_price, product_rating
 
 
-    def create_product_dict(self, product_name, product_price, product_rating) -> dict:     
+    def _create_product_dict(self, product_name, product_price, product_rating) -> dict:     
         '''
         Creates a product dictionary using the below parameters.
 
         Parameters:
         -----------
         product_name:
-            the name of the product
+            The name of the product.
 
         product_price:
-            the price of the product
+            The price of the product.
 
         product_rating:
-            the customer review rating of the product
+            The customer review rating of the product.
         '''
         product_dict = {}
 
@@ -251,14 +254,14 @@ class MyProteinScraper(Webscraper):
             "Product Name" : product_name,
             "Price" : product_price,
             "Rating" : product_rating,
-            "Time Scraped" : self.get_timestamp(),
-            "Image Link" : self.get_product_image()            
+            "Time Scraped" : self._get_timestamp(),
+            "Image Link" : self._get_product_image()            
             })
 
         return product_dict
 
 
-    def create_product_folder(self, filename):
+    def _create_product_folder(self, filename):
         '''
         Creates a folder called 'raw_data' if it doesn't already exist, and then creates a folder within that, with the unique product ID as the filename.
 
@@ -274,83 +277,85 @@ class MyProteinScraper(Webscraper):
             os.makedirs(f'raw_data/{filename}')
 
 
-    def write_json(self, data, filename):
+    def _write_json(self, data, filename):
         '''
         Writes the dictionary data to a json file and saves it within it's own product folder.
 
         Parameters:
         -----------
         data:
-            the product dictionary to be saved into the json format
+            The product dictionary to be saved into the json format.
         
         filename:
-            the unique product ID to be used as a folder name.
+            The unique product ID.
         '''
         with open(f'raw_data/{filename}/data.json', 'w') as file:
             json.dump(data, file, indent = 4)   #indent = 4 makes the data more readable
 
 
-    def create_image_folder(self, filename):
+    def _create_image_folder(self, filename):
         '''
         Creates 'images' folder if it doesn't already exist.
         
         Parameters:
         -----------
         filename:
-            the unique product ID to be used as a folder name.
+            The unique product ID.
         '''
         if not os.path.exists(f'raw_data/{filename}/images'):
             os.makedirs(f'raw_data/{filename}/images')
 
 
-    def download_image(self, image_src, filename):
+    def _download_image(self, image_src, filename):
         '''
         Downloads and saves the relevant .jpg image within it with the product ID as the filename.
 
         Parameters:
         ----------
         image_src:
-            the URL of the image to be downloaded
+            The URL of the image to be downloaded.
+        
+        filename:
+            The unique product ID.
         '''
         image_src = requests.get(image_src).content
-
-        image_filename = self.get_date_and_timestamp()
+        image_filename = self._get_date_and_timestamp()
 
         with open(f'raw_data/{filename}/images/{image_filename}.jpg', 'wb') as file:     #wb means file is opened for writing in binary mode.
             file.write(image_src)
 
 
-
-
-
     def crawler(self, product_links):
         '''
-        Web scraper for one page... Iterates through URL links on webpage and scrapes and saves relevant product and image data from each.
+        THe webscraper for one webpage, which iterates through URL links to find and save relevant product and image data from each.
+        
+        Parameters:
+        ----------
+        product_links:
+            The list of product URLs to all products on the webpage.
         '''
         product_data_list_all= []                       #list of product dictionaries
-        #product_links = self.find_product_links()
         
         #for link in range(len(product_links)): 
-        for link in range(0,1):                         #for testing (be careful when removing as this will download ALL images - space on harddrive)
+        for link in range(0,2):                         #for testing (be careful when removing as this will download ALL images - space on harddrive)
 
-            #product_links = self.find_product_links()
             product_link = product_links[link]
             self.driver.get(product_link)
             time.sleep(1)
 
-            product_data = self.get_product_data()
-            product_dict = self.create_product_dict(product_data[0], product_data[1], product_data[2])
+            product_data = self._get_product_data()
+            product_dict = self._create_product_dict(product_data[0], product_data[1], product_data[2])
 
-            filename = list(product_dict.values())[0]   #indexes the product ID value and uses it for folder name   
+            filename = list(product_dict.values())[0]    #indexes the product ID value and uses it for folder name   
 
-            self.create_product_folder(filename)
-            self.write_json(product_data, filename)     #writes the dictionary to a json file within the folder created above
+            self._create_product_folder(filename)
+            self._write_json(product_data, filename)     #writes the dictionary to a json file within the folder created above
 
             product_data_list_all.append(product_data)
 
-            #self.create_image_folder(filename)
-            #image = self.get_product_image()
-            #self.download_image(image, filename)
+            self._create_image_folder(filename)
+            image = self._get_product_image()
+            self._download_image(image, filename)
         
         return product_data_list_all
 
@@ -361,13 +366,13 @@ if __name__ == "__main__":
 
     scrape.close_email_signup()
     scrape.accept_cookies()
-    scrape.nutrition_button_click()
-    scrape.open_all_nutrition_products()
+    scrape._nutrition_button_click()
+    scrape._open_all_nutrition_products()
     
     pages = scrape.driver.find_element(By.XPATH, '//li/a[@class="responsivePaginationButton responsivePageSelector   responsivePaginationButton--last"]').text
 
     for page in range(1, int(pages) + 1):
-        links = scrape.find_product_links()
+        links = scrape._find_product_links()
         time.sleep(2)
 
         scrape.crawler(links)
